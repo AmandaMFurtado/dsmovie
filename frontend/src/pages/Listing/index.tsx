0bc4e6c459065dc8d82dba44afc14a4a84224ff7
Amanda Marques <andaemc@gmail.com>
2022-01-17 00:03:58 -0300

    Save, useNavigate

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,59 +1,52 @@
-
 import axios from "axios";
+import { useState, useEffect } from "react";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
-import { useEffect, useState } from "react";
+import { BASE_URL } from "utils/request";
 import { MoviePage } from "types/movie";
-import {BASE_URL} from "utils/request";
 
+function Listing() {
 
-function Listing(){
+    const [pageNumber, setPageNumber] = useState(0);
 
+    const [page, setPage] = useState<MoviePage>({
+        content: [],
+        last: true,
+        totalPages: 0,
+        totalElements: 0,
+        size: 12,
+        number: 0,
+        first: true,
+        numberOfElements: 0,
+        empty: true
+    });
 
-        const [pageNumber,setPageNumber] = useState(0);
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`)
+            .then(response => {
+                const data = response.data as MoviePage;
+                setPage(data);
+            });
+    }, [pageNumber]);
 
-        const [page, setPage] = useState<MoviePage>({
-                content:[],
-                 last: true,
-                totalPages: 0,
-                totalElements: 0,
-                size: 12,
-                number: 0,
-                first: true,
-                numberOfElements: 0,
-                empty: true
-         });
+    const handlePageChange = (newPageNumber : number) => {
+        setPageNumber(newPageNumber);
+    }
 
-        useEffect(() => {
-                axios.get(`${BASE_URL}/movies?size=12page=${pageNumber}`)
-                .then(response => {
-                        const data = response.data as MoviePage;
-                        setPage(data);
-                });
-        },[pageNumber]);
-        
-        const handlePageChange = (newPageNumber : number) => {
-                setPageNumber(newPageNumber);
-        }
+    return (
+        <>
+            <Pagination page={page} onChange={handlePageChange} />
 
-        return (
-            <>
-        <Pagination page={page}onChange={handlePageChange}/>
-        <div className='container'>
-           <div className = 'row'>
-                {page.content.map(movie => (
-                                <div key = {movie.id} className='col-sm-6 col-lg-4 col-xl-3 mb-3'>
-                 <MovieCard movie={movie}/>
-         </div>
-                        )
-                        
-               )}
-
-              
-   
-    </div>
-</div>
-
+            <div className="container">
+                <div className="row">
+                    {page.content.map(movie => (
+                        <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                            <MovieCard movie={movie} />
+                        </div>
+                    )
+                    )}
+                </div>
+            </div>
         </>
     );
 }
